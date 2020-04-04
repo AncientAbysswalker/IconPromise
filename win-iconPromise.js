e53e95d590d5a3f228de1048380fcf7f770d110f
Ancient Abysswalker <ancientabysswalker@gmail.com>
2020-04-04 10:23:39 -0700

    >Added docstrings >Updated getIcon() method arguments to match new .NET code >Added additional methods to fix size for 16, 32, 48, or 256 px

--- win-iconPromise.js
+++ win-iconPromise.js
@@ -5,8 +5,22 @@ let path = require('path');
 let _ = require('lodash');
 
 function IconPromise(){
-  this.getIcon = function(path, context = "No Context Provided"){
-    let iconProcess = child_process.spawn(getPlatformIconProcess(), ['-x']);
+
+  /**
+   * Extracts image data from a file's icon and provides it wrapped in a
+   *     promise, with an optional argument that allows for the adjustment of
+   *     size. Default size is 32x32 pixels.
+   * @param {string} path The file path for the document that we would like to
+   *     extract the icon data of.
+   * @param {string} sizeArg The size argument string to be passed through to
+   *     the .NET framework code
+   * @param {string} context A context string that will be returned with the
+   *     JSON object to lend insight into the function caller
+   * @return {Promise} Returns a promise for a javascript object that contains the
+   *     icon image data.
+   */
+  this.getIcon = function(path, sizeArg = '--size-32', context = "No Context Provided"){
+    let iconProcess = child_process.spawn(getPlatformIconProcess(), ['--x', sizeArg]);
 
     // Initialize buffer
     let iconDataBuffer = "";
@@ -59,9 +73,65 @@ function IconPromise(){
     });
   }
 
+  /**
+   * Extracts image data from a file's icon and provides it wrapped in a
+   *     promise, with the size set as 16x16 pixels.
+   * @param {string} path The file path for the document that we would like to
+   *     extract the icon data of.
+   * @param {string} context A context string that will be returned with the
+   *     JSON object to lend insight into the function caller
+   * @return {Promise} Returns a promise for a javascript object that contains the
+   *     icon image data.
+   */
+  this.getIcon16 = function(path, context = "No Context Provided"){
+    return this.getIcon(path, '--size-16', context);
+  }
+
+  /**
+   * Extracts image data from a file's icon and provides it wrapped in a
+   *     promise, with the size set as 32x32 pixels.
+   * @param {string} path The file path for the document that we would like to
+   *     extract the icon data of.
+   * @param {string} context A context string that will be returned with the
+   *     JSON object to lend insight into the function caller
+   * @return {Promise} Returns a promise for a javascript object that contains the
+   *     icon image data.
+   */
+  this.getIcon32 = function(path, context = "No Context Provided"){
+    return this.getIcon(path, '--size-32', context);
+  }
+
+  /**
+   * Extracts image data from a file's icon and provides it wrapped in a
+   *     promise, with the size set as 48x48 pixels.
+   * @param {string} path The file path for the document that we would like to
+   *     extract the icon data of.
+   * @param {string} context A context string that will be returned with the
+   *     JSON object to lend insight into the function caller
+   * @return {Promise} Returns a promise for a javascript object that contains the
+   *     icon image data.
+   */
+  this.getIcon48 = function(path, context = "No Context Provided"){
+    return this.getIcon(path, '--size-48', context);
+  }
+
+  /**
+   * Extracts image data from a file's icon and provides it wrapped in a
+   *     promise, with the size set as 256x256 pixels.
+   * @param {string} path The file path for the document that we would like to
+   *     extract the icon data of.
+   * @param {string} context A context string that will be returned with the
+   *     JSON object to lend insight into the function caller
+   * @return {Promise} Returns a promise for a javascript object that contains the
+   *     icon image data.
+   */
+  this.getIcon256 = function(path, context = "No Context Provided"){
+    return this.getIcon(path, '--size-256', context);
+  }
+
   function getPlatformIconProcess(){
     if(os.type() == 'Windows_NT'){
-      return path.join(__dirname,'/bin/IconPromise.exe');
+      return path.join(__dirname,'/bin/IconExtractor.exe');
       //Do stuff here to get the icon that doesn't have the shortcut thing on it
     } else {
       throw('This platform (' + os.type() + ') is unsupported =(');
